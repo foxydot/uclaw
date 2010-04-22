5ebf9ebd0d880ca8ffca49f5f596805249f49382
Catherine Sandrick <csandrick@palindrome.local>
2010-04-22 19:29:46 -0400

    getting started

---
+++ sites/all/themes/uclaw/page_rps.tpl.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+
+  <head>
+    <title><?php print $head_title; ?></title>
+    <?php print $head; ?>
+    <?php print $styles; ?>
+    <!--[if lte IE 6]><style type="text/css" media="all">@import "<?php print $base_path . path_to_theme() ?>/css/ie6.css";</style><![endif]-->
+    <!--[if IE 7]><style type="text/css" media="all">@import "<?php print $base_path . path_to_theme() ?>/css/ie7.css";</style><![endif]-->
+    <?php print $scripts; ?>
+  </head>
+
+  <body class="<?php print $body_classes; ?>">
+
+<div id="container">
+	<div id="wrapper">
+		<div id="subnav">
+		<!-- SUBNAV -->
+			
+			<ul>
+				<li><a href="/">HOME</a></li>
+				<li>|</li>
+                <li><a href="https://wiki.ucfilespace.uc.edu/groups/lawclassupdates/" target="_blank">CLASS UPDATES</a></li>
+				<li>|</li>
+				<li><a href="/current/resources/directories.shtml">DIRECTORY</a></li>
+				<li>|</li>
+				<li><a href="/prospective/admissions/visit/directions.shtml">DIRECTIONS</a></li>
+				<li>|</li>
+				<li><a href="/calendar">CALENDAR</a></li>
+			</ul>
+		<!-- SUBNAV -->
+		</div>
+		<div id="header">
+			<!-- HEADER -->
+			<h1><a href="http://www.uc.edu" target="_blank">Law School</a></h1>
+            <div id="search">
+                 <?php print $search_box; ?>		
+            </div>
+			<ul>
+				<li class="focusAlt"><a href="/prospective">Prospective Students</a></li>
+				<li class="focusAlt"><a href="/current ">Current Students</a></li>
+				<li class="nav"><a href="/alumni">Alumni</a></li>
+				<li>|</li>
+				<li class="nav"><a href="/faculty">Faculty & Staff</a></li>
+				<li>|</li>
+				<li class="nav"><a href="/institutes">Institutes & Centers</a></li>
+				<li>|</li>
+				<li class="nav"><a href="/library">Library</a></li>
+				<li>|</li>
+				<li class="nav"><a href="/blogs/">Blogs</a></li>
+				<li>|</li>		
+				<li class="nav"><a href="/about">About Us</a></li>
+			</ul>
+		</div>
+		<div id="bannerHome"><script type="text/javascript" src="http://law.uc.edu/assets/js/randHome.js"></script> </div>
+		<div id="content">
+			<div class="subColumnA">
+				<a href="/alumni/#tabview=tab3"><img src="http://law.uc.edu/assets/images/production/home/headers/headerSupport.jpg" alt="Support Your School"/></a>
+				<div class="wrapperColumnHome">
+					<h6><a href="http://www.law.uc.edu/alumni/support/campaign.shtml"><img src="http://law.uc.edu/assets/images/production/home/featured/cornerstone_sm.jpg" alt="cornerstone" width="30" height="31" align="top" class="floatLeft" /></a> Cornerstones: Building the Future</h6>
+                    <br />
+                    <br />
+				  <h6>Give to Proudly Cincinnati</h6>
+					<p><strong><em>Proudly Cincinnati,</em></strong> UC's most ambitious fundraising campaign in history, has passed its halfway point.  <a href="http://www.uc.edu/proudlycincinnati/">Learn more.</a>				</p>
+				</div>	
+				<div class="photoThumb"><a href="http://www.uc.edu/proudlycincinnati/"><img src="http://law.uc.edu/assets/images/production/home/featured/proudly.jpg" alt="Support Your School" border="0"/></a></div>
+			</div>
+			<div class="subColumnA">
+				<a href="/news/news/"><img src="http://law.uc.edu/assets/images/production/home/headers/headerNews.jpg" alt="News"/></a>
+				<div class="wrapperColumnHome">
+                
+                <h6>Professor Jacob Katz Cogan Receives Prestigious Award</h6>
+                <p><a href="news/news/cogan.shtml">Professor Cogan is a co-recipient of the American Society of International Law’s 2010 Francis Deák Prize.</a></p>
+                <h6>DNA Bill Helped by UC Law Students</h6>
+                <p><a href="http://www.uc.edu/news/NR.aspx?id=11670">Governor Strickland signs reform bill, developed by OIP students..."</a></p>
+                <h6>&nbsp;</h6>
+</div>	
+				<div class="photoThumb"><a href="http://www.uc.edu/News/NR.aspx?id=11573"><img src="http://law.uc.edu/assets/images/production/home/featured/gavel.jpg" alt="News"/></a></div>
+			</div>
+			<div class="subColumnA">
+				<a href="/news/events/"><img src="http://law.uc.edu/assets/images/production/home/headers/headerEvents.jpg" alt="Events"/></a>
+				<div class="wrapperColumnHome">
+
+
+<h6>April 15, 2010</h6>
+<p><a href="http://www.law.uc.edu/news/events/quasi.shtml">Quasi-crime and Quasi-punishment...with Prof. Jack Chin</a></p>
+
+<h6>April 14-16, 2010</h6>
+<p><a href="http://www.law.uc.edu/news/events/barkett.shtml">Judge in Residence Program</a></p>
+
+<p><a href="news/events/chesley2010.shtml">View webcast of the 2010 Chesley Distinguished Visitor Professor Lawrence Lessig</a></p>
+
+</div>
+				<div class="photoThumb"><a href="http://www.law.uc.edu/news/events/barkett.shtml"><img src="http://law.uc.edu/assets/images/production/home/featured/barkett.jpg" alt="UC College of Law Events"/></a></div>
+			</div>
+			<div class="subColumnA"> <a href="news/news/osuch.shtml"><img src="http://law.uc.edu/assets/images/production/home/headers/headerAcademics.jpg" alt="Academics"/></a>
+				<div class="wrapperColumnHome">
+					<img src="http://law.uc.edu/assets/images/production/global/icons/studentSpotlight.jpg" alt="UC College of Law Academics" class="floatLeft"/>
+			    <h5>Student  Spotlight</h5>
+				  <p>Adrianne Caldwell '10</p>
+					<p>Law student Adrianne Caldwell &rsquo;10  came to UC Law to try something new. After living in North Carolina her entire life, she decided  she needed a different experience. <a href="news/news/caldwell.shtml" target="blank"> Read More</a></a></p>
+
+
+				</div>	
+				<div class="photoThumb"><a href="news/news/caldwell.shtml" target="blank"><img src="http://law.uc.edu/assets/images/production/home/featured/caldwell.jpg" alt="UC College of Law Academics" /></a></div>
+			</div>	
+			<div class="clear"></div>	
+		</div>
+	</div>	
+</div>
+<div id="footer">
+	<ul>
+		<li class="highlight">&copy; University of Cincinnati 2010.</li>
+		<li>All Rights Reserved.</li>
+	</ul>
+	<ul>
+		<li>PO Box 210040</li>
+		<li>Clifton Avenue & Calhoun Street</li>
+		<li>Cincinnati, OH 45221-0040</li>
+	</ul>
+	<ul>
+		<li>513-556-6805 (p) / 513-556-2391 (f)</li>
+		<li><a href="mailto:webmaster@law.uc.edu">webmaster@law.uc.edu</a></li>
+	</ul>
+	<div class="clear"></div>
+</div>
+
+ <?php print $right; ?> <?php print $left; ?>	 <?php print $content; ?>	
+
+</body>
+
+</html>
