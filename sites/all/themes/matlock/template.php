200d9e15272dbe92cf84740808421572ce2a00a5
Josh Toth <josh@roundpixelstudio.com>
2013-07-12 16:17:41 -0400

    Started basic styling/theming

--- sites/all/themes/matlock/template.php
+++ sites/all/themes/matlock/template.php
@@ -3,3 +3,9 @@
 /**
  * @file template.php
  */
+
+ // Typekit Reference
+ function matlock_preprocess_html(&$variables) {
+ 	drupal_add_js('//use.typekit.net/joq3zvd.js');
+ 	drupal_add_js('try{Typekit.load();}catch(e){}', 'inline');
+ }
--- sites/all/themes/matlock/templates/footer.tpl.php
+++ sites/all/themes/matlock/templates/footer.tpl.php
@@ -1,53 +1,54 @@
 <footer class="footer">
-		<div class="container">
-				<div class="row">
-					<div class="span12">
-						<div id="links" class="row">
-							<div class="span2">
-								<h5>Resources</h5>
-								<ul>
-							        <li><a href="http://law.uc.edu/prospective-students/admissions/directions">Directions</a></li>
-									<li><a href="http://www.law.uc.edu/sites/default/files/WestCampus-July2006-clr.pdf" target="_blank">Maps</a></li>
-									<li><a href="http://law.uc.edu/alumni/support ">Giving</a></li>
-							    </ul>
-							</div>
-							<div class="span2">
-								<h5>Get Started</h5>
-								<ul>
-  									<li><a href="#welcomevideo" class="button" data-toggle="modal">Welcome Message</a></li>
-  									<li><a href="http://law.uc.edu/advantage  http://www.cincinnatiusa.com/about/ ">Explore Cincinnati</a></li>
-									<li><a href="http://www.law.uc.edu/prospective-students/admissions/visit-college-law ">Schedule A Visit</a></li>
-									<li><a href="http://www.law.uc.edu/prospective-students/admissions/application-materials-fall">Apply Now</a></li>
-					            </ul>
-							</div>
-							<div class="span3 offset2">
-								<h5>Connect With Us</h5>
-								<ul id="social" class="inline">
-					    			<li><a class="ss-facebook" href="http://www.facebook.com/#!/pages/University-of-Cincinnati-College-of-Law/318653248160979?fref=ts"></a></li>
-									<li><a class="ss-twitter" href="https://twitter.com/CincyLawCPD"></a></li>
-									<li><a class="ss-youtube" href="http://www.youtube.com/user/UCincinnatiLaw"></a></li>
-									<li><a class="ss-linkedin" href="http://www.linkedin.com/groups/University-Cincinnati-College-Law-Students-1033937?goback=%2Egdr_1247502868379_1%2Egna_1033937"></a></li>
-									<li><a class="ss-rss" href="#"></a></li>
-					    		</ul>
-							</div>
-							<div class="span3">
-								<h5>Contact Us</h5>
-								<address>
-								  <strong>UC College of Law</strong><br>
-									PO Box 210040<br>
-									Clifton Avenue & Calhoun Street<br>
-									Cincinnati, OH 45221-0040<br>
-									Fax: (585) 641-8070  <br>
-									<abbr title="Phone">P: </abbr>513-556-6805<br />
-									<abbr title="Fax">F: </abbr>513-556-2391<br />
-									<abbr title="Email">E: </abbr><a href="mailto:webmaster@law.uc.edu">webmaster@law.uc.edu</a>
-								</address>
+	<div class="container">
+		<div class="row">
+			<div class="span12">
+				<div id="links" class="row">
+					<div class="span2">
+						<h5>Resources</h5>
+						<ul>
+					        <li><a href="http://law.uc.edu/prospective-students/admissions/directions">Directions</a></li>
+							<li><a href="http://www.law.uc.edu/sites/default/files/WestCampus-July2006-clr.pdf" target="_blank">Maps</a></li>
+							<li><a href="http://law.uc.edu/alumni/support ">Giving</a></li>
+					    </ul>
+					</div>
+					<div class="span2">
+						<h5>Get Started</h5>
+						<ul>
+								<li><a href="#welcomevideo" class="button" data-toggle="modal">Welcome Message</a></li>
+								<li><a href="http://law.uc.edu/advantage  http://www.cincinnatiusa.com/about/ ">Explore Cincinnati</a></li>
+							<li><a href="http://www.law.uc.edu/prospective-students/admissions/visit-college-law ">Schedule A Visit</a></li>
+							<li><a href="http://www.law.uc.edu/prospective-students/admissions/application-materials-fall">Apply Now</a></li>
+			            </ul>
+					</div>
+					<div class="span3 offset2">
+						<h5>Connect With Us</h5>
+						<ul id="social" class="inline">
+			    			<li><a class="ss-facebook" href="http://www.facebook.com/#!/pages/University-of-Cincinnati-College-of-Law/318653248160979?fref=ts"></a></li>
+							<li><a class="ss-twitter" href="https://twitter.com/CincyLawCPD"></a></li>
+							<li><a class="ss-youtube" href="http://www.youtube.com/user/UCincinnatiLaw"></a></li>
+							<li><a class="ss-linkedin" href="http://www.linkedin.com/groups/University-Cincinnati-College-Law-Students-1033937?goback=%2Egdr_1247502868379_1%2Egna_1033937"></a></li>
+							<li><a class="ss-rss" href="#"></a></li>
+			    		</ul>
+					</div>
+					<div class="span3">
+						<h5>Contact Us</h5>
+						<address>
+						  <strong>UC College of Law</strong><br>
+							PO Box 210040<br>
+							Clifton Avenue & Calhoun Street<br>
+							Cincinnati, OH 45221-0040<br>
+							Fax: (585) 641-8070  <br>
+							<abbr title="Phone">P: </abbr>513-556-6805<br />
+							<abbr title="Fax">F: </abbr>513-556-2391<br />
+							<abbr title="Email">E: </abbr><a href="mailto:webmaster@law.uc.edu">webmaster@law.uc.edu</a>
+						</address>
 
-							</div>
-						</div>
-						<p class="copyright">&copy; University of Cincinnati 2013. All Rights Reserved.</p>
 					</div>
 				</div>
+				<p class="copyright">&copy; University of Cincinnati 2013. All Rights Reserved.</p>
 			</div>
-  <?php print render($page['footer']); ?>
+		</div>
+	</div>
+
+  <?php // Causes PHP notice - print render($page['footer']); ?>
 </footer>
