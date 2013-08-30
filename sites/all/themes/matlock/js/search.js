a8b7fc6a13920c9a0af59428da85bbe0243893f2
Josh Toth <josh@roundpixelstudio.com>
2013-08-30 18:22:42 -0400

    Fix sidr search

--- sites/all/themes/matlock/js/search.js
+++ sites/all/themes/matlock/js/search.js
@@ -7,13 +7,8 @@
 			window.location = '/search/node/' + term;
 		});		
 
-		$('#sidr').on('submit', 'form', function(e) {
-			e.preventDefault();
-			alert('submitting');
-			var term = $(this).find('input[type="text"]').val();
-			window.location = '/search/node/' + term;
-		});		
-
-		$('#sidr').css('border', '3px solid red');
+		$('#sidr-id-header-search').on('keypress', function() {
+			console.log('pressed');
+		});
 	});
 })(jQuery);
